71442fdade9841840e5a70085f59abb655b51131
sumaira sadiq <sumairasadiq482@gmail.com>
2023-10-29 13:12:56 -0700

    third comments

---
+++ python_with_Rehan/10_funcations.py
@@ -0,0 +1,23 @@
+
+#funcation  defination
+def print_codanics():
+    print("we are learning")
+    print("we are learning")
+    print("we are learning")
+
+    print_codanics()
+
+    # 2 funcation
+    def print_codanics():
+     text = "we are learning"
+     print(text)
+     print(text)
+     print(text)
+
+    print_codanics()
+
+
+
+
+
+
